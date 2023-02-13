01a6d3e6ec76f8cecd6f86bf431a952887b277b5
Kormos Csanad <csanadsuli19@gmail.com>
2023-02-13 16:41:36 +0100

    -update User functions

--- backend/src/user/dto/updateUserDTO.ts
+++ backend/src/user/dto/updateUserDTO.ts
@@ -1,4 +1,8 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class UpdateUserDTO {
+  @IsEmail()
   email?: string;
+  @IsNotEmpty()
   password?: string;
 }
--- backend/src/user/service/user.service.ts
+++ backend/src/user/service/user.service.ts
@@ -1,1 +1,91 @@
-import {Injectable} from '@nestjs/common';import {PrismaService} from '../../utils/prirsma.service';import {CreateUserDTO} from '../dto/createUserDTO';import {Prisma} from "@prisma/client";import {Roles} from "../utils/roles";import {encryptData} from "../../utils/bcrypt";@Injectable()export class UserService {  constructor(private prismaService: PrismaService) {}  async getUsers() {    const users = await this.prismaService.users.findMany();    console.log(users);    return users;  }  async getUserByEmail(email: string) {    const users = await this.prismaService.users.findUnique({      where: {        email      },      include: {        roles: true,      }    });    return users;  }  async getUserById(userId: number) {    const users = await this.prismaService.users.findUnique({      where: {        userId      },      include: {        roles: true,      }    });    return users;  }  /*async createUsersWithRoles() {    const users = await this.prismaService.users.createMany({      data: [        { email: 'csanademail@gmail.com', password: 'myPassword', roleId: 3 },        {          email: 'NimrodEmail@asd.hu',          password: 'mySecondPassword',          roleId: 3,        },      ],    });  }*/  getRoleId(isCoach: boolean): number{      return isCoach? Roles.COACH.roleId: Roles.USER.roleId;  }  getUsersCreateInput(user: CreateUserDTO): Prisma.usersCreateInput {    const roleId = this.getRoleId(user.isCoach);    return {      email: user.email,      password: encryptData(user.password),      roles: {        connect: {          roleId        }      }    };  }}
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../utils/prirsma.service';
+import { CreateUserDTO } from '../dto/createUserDTO';
+import { Prisma } from '@prisma/client';
+import { Roles } from '../utils/roles';
+import {compareData, encryptData} from '../../utils/bcrypt';
+import { UpdateUserDTO } from '../dto/updateUserDTO';
+
+@Injectable()
+export class UserService {
+  constructor(private prismaService: PrismaService) {}
+  async getUsers() {
+    const users = await this.prismaService.users.findMany();
+    console.log(users);
+    return users;
+  }
+  async getUserByEmail(email: string) {
+    const users = await this.prismaService.users.findUnique({
+      where: {
+        email,
+      },
+      include: {
+        roles: true,
+      },
+    });
+    return users;
+  }
+  async getUserById(userId: number) {
+    const users = await this.prismaService.users.findUnique({
+      where: {
+        userId,
+      },
+      include: {
+        roles: true,
+      },
+    });
+    return users;
+  }
+
+  /*async createUsersWithRoles() {
+    const users = await this.prismaService.users.createMany({
+      data: [
+        { email: 'csanademail@gmail.com', password: 'myPassword', roleId: 3 },
+        {
+          email: 'NimrodEmail@asd.hu',
+          password: 'mySecondPassword',
+          roleId: 3,
+        },
+      ],
+    });
+  }*/
+  getRoleId(isCoach: boolean): number {
+    return isCoach ? Roles.COACH.roleId : Roles.USER.roleId;
+  }
+  getUsersCreateInput(user: CreateUserDTO): Prisma.usersCreateInput {
+    const roleId = this.getRoleId(user.isCoach);
+    return {
+      email: user.email,
+      password: encryptData(user.password),
+      roles: {
+        connect: {
+          roleId,
+        },
+      },
+    };
+  }
+  async getUsersUpdateInput(
+    userDTO: UpdateUserDTO,
+    userId: number,
+  ): Promise<Prisma.usersUpdateInput> {
+    const user = await this.getUserById(userId);
+
+    const userUpdateInput: Prisma.usersUpdateInput = {
+      email: user.email,
+      password: user.password,
+    };
+    if (userDTO.email) {
+      if (userDTO.email !== user.email) {
+        userUpdateInput.email = userDTO.email;
+        return userUpdateInput;
+      }
+    }
+    if (userDTO.password) {
+      if (!compareData(userDTO.password, user.password)) {
+        userUpdateInput.password = encryptData(userDTO.password);
+        return userUpdateInput;
+      }
+    }
+    return userUpdateInput;
+  }
+}
