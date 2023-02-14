5541d324db14e5a7a0168ab5edf94c9c93c531a5
Somogyi Botond <somogyi.botond.munka@gmail.com>
2023-02-14 09:04:31 +0100

    tokens and auth guard

--- backend/src/auth/guards/local-auth.guard.ts
+++ backend/src/auth/guards/local-auth.guard.ts
@@ -1,8 +1,1 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-@Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {
-    
-
-}
+import { Injectable } from '@nestjs/common';import { AuthGuard } from '@nestjs/passport';@Injectable()export class LocalAuthGuard extends AuthGuard('local') {}
