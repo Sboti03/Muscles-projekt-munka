a2501911bc4a9976b7f785a8c13aea0b951f578d
Somogyi Botond <113022915+Sboti03@users.noreply.github.com>
2023-02-18 00:04:22 +0100

    Line breaks

--- backend/src/auth/guards/local-auth.guard.ts
+++ backend/src/auth/guards/local-auth.guard.ts
@@ -1,1 +1,9 @@
-import { Injectable } from '@nestjs/common';import { AuthGuard } from '@nestjs/passport';@Injectable()export class LocalAuthGuard extends AuthGuard('local') {}
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class LocalAuthGuard extends AuthGuard('local') {
+
+
+
+}
