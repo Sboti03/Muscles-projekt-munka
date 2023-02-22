ded6ec90f9bfbacae069ec445d58ed8a6d7e871d
Morgován Nimród <morgovannimrod22@gmail.com>
2023-02-22 16:20:03 +0100

    restructuring folders and formatting lines in weight history

--- backend/src/weight-history/services/weight-history-update-or-create/weight-history-update-or-create.service.spec.ts
+++ backend/src/weight-history/services/weight-history-update-or-create/weight-history-update-or-create.service.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { WeightHistoryUpdateOrCreateService } from './weight-history-update-or-create.service';
+import {Test, TestingModule} from '@nestjs/testing';
+import {WeightHistoryUpdateOrCreateService} from './weight-history-update-or-create.service';
 
 describe('WeightHistoryUpdateService', () => {
-  let service: WeightHistoryUpdateOrCreateService;
+    let service: WeightHistoryUpdateOrCreateService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [WeightHistoryUpdateOrCreateService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [WeightHistoryUpdateOrCreateService],
+        }).compile();
 
-    service = module.get<WeightHistoryUpdateOrCreateService>(WeightHistoryUpdateOrCreateService);
-  });
+        service = module.get<WeightHistoryUpdateOrCreateService>(WeightHistoryUpdateOrCreateService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
