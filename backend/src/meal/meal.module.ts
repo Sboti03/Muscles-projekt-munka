218b79958152245cf98be0261339728888ee42af
Kormos Csanad <csanadsuli19@gmail.com>
2023-02-22 11:55:17 +0100

    module Update

--- backend/src/meal/meal.module.ts
+++ backend/src/meal/meal.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MealCreateService } from './services/meal-create/meal-create.service';
 import {MealController} from "./controller/meal.controller";
+import {MealConvertService} from "./services/meal-convert/meal-convert.service";
+import {MealUpdateService} from "./services/meal-update/meal-update.service";
+import {MealDeleteService} from "./services/meal-delete/meal-delete.service";
 
 @Module({
   exports: [MealModule],
-  providers: [MealCreateService],
+  providers: [MealCreateService,
+    MealConvertService,
+    MealUpdateService,
+    MealDeleteService],
   controllers: [MealController]
 })
 export class MealModule {}
