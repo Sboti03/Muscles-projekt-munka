48f8415470fb7855eb449bac69767cfdab3e1cfb
Morgován Nimród <morgovannimrod22@gmail.com>
2023-02-22 15:29:00 +0100

    updating DTO with validation

--- backend/src/weight-history/WeightHistoryData.dto.ts
+++ backend/src/weight-history/WeightHistoryData.dto.ts
@@ -1,4 +1,13 @@
+import {Type} from "class-transformer";
+import {IsDate, IsNumber} from "class-validator";
+
 export class WeightHistoryDataDto {
+
+    @Type(() => Date)
+    @IsDate()
     date: Date;
+
+    @Type(() => Number)
+    @IsNumber()
     weight: number;
 }
