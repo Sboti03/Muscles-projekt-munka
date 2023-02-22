a633ef6290b6ae294b2db914c1a8044d15a2b5b1
Morgován Nimród <morgovannimrod22@gmail.com>
2023-02-22 09:09:19 +0100

    create DayHistory

--- backend/src/day-history/services/day-history.service.ts
+++ backend/src/day-history/services/day-history.service.ts
@@ -1,8 +1,25 @@
-import {Injectable} from '@nestjs/common';
+import {Body, Injectable} from '@nestjs/common';
+import {GetCurrentUser} from "../../auth/decorators/decorators";
+import {DayHistoryCreateDto} from "../day-history-create-dto/DayHistoryCreate.dto";
+import {PrismaService} from "../../utils/prirsma.service";
 
 @Injectable()
 export class DayHistoryService {
-    createDayHistory() {
-        
+    constructor(private prismaService: PrismaService) {
     }
+
+    createDayHistory(profileId: number, date: Date) {
+        this.prismaService.dayHistory.create({
+            data: {
+                profileData: {
+                    connect: {
+                        profileId: profileId,
+                    },
+                },
+                date: date,
+            },
+        });
+    }
+
+
 }
