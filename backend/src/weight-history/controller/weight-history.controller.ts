20dd691eeef7eeba6c4bd456873a00d829fe4165
Morgován Nimród <morgovannimrod22@gmail.com>
2023-02-22 16:06:38 +0100

    creating weight-history controller

--- backend/src/weight-history/controller/weight-history.controller.ts
+++ backend/src/weight-history/controller/weight-history.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, NotFoundException, Post} from '@nestjs/common';
+import {PrismaService} from "../../utils/prirsma.service";
+import {GetCurrentUserProfileId} from "../../auth/decorators/decorators";
+import {DayHistoryCreateService} from "../../day-history/services/day-history-create/day-history-create.service";
+import {
+    WeightHistoryUpdateOrCreateService
+} from "../services/weight-history-update-or-create/weight-history-update-or-create.service";
+import {DayHistoryGetService} from "../../day-history/services/day-history-get/day-history-get.service";
+import {WeightHistoryDataDto} from "../WeightHistoryData.dto";
 
 @Controller('weight-history')
-export class WeightHistoryController {}
+export class WeightHistoryController {
+
+    constructor(private prismaService: PrismaService,
+                private dayHistory: DayHistoryCreateService,
+                private weightHistoryUpdateOrCreateService: WeightHistoryUpdateOrCreateService,
+                private dayHistoryGetService: DayHistoryGetService) {
+    }
+
+
+    @Post('/update')
+    async createOrUpdate(@Body() weightHistoryData: WeightHistoryDataDto, @GetCurrentUserProfileId() currentProfileId) {
+        let dayId: number;
+        if (currentProfileId !== -1) {
+            try {
+                dayId = (await this.dayHistory.createDayHistory(currentProfileId, weightHistoryData.date)).dayId;
+            } catch (e) {
+                dayId = (await this.dayHistoryGetService.getDayIdByDate(weightHistoryData.date, currentProfileId)).dayId;
+            }
+            await this.weightHistoryUpdateOrCreateService.updateOrCreateWeightHistory(weightHistoryData.weight, dayId);
+        } else {
+            throw new NotFoundException();
+        }
+    }
+}
