555af82d59e2e531d67c28160eb6732d1018e3c6
Kormos Csanad <csanadsuli19@gmail.com>
2023-02-22 11:57:11 +0100

    module Update

--- backend/src/meal/meal.module.ts
+++ backend/src/meal/meal.module.ts
@@ -4,13 +4,15 @@ import {MealController} from "./controller/meal.controller";
 import {MealConvertService} from "./services/meal-convert/meal-convert.service";
 import {MealUpdateService} from "./services/meal-update/meal-update.service";
 import {MealDeleteService} from "./services/meal-delete/meal-delete.service";
+import {PrismaService} from "../utils/prirsma.service";
 
 @Module({
   exports: [MealModule],
   providers: [MealCreateService,
     MealConvertService,
     MealUpdateService,
-    MealDeleteService],
+    MealDeleteService,
+    PrismaService],
   controllers: [MealController]
 })
 export class MealModule {}
