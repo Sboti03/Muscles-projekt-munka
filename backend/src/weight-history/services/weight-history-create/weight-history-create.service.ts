a8040423dbbd28d448d44f7ed503e1e5ff3c9ce4
Morgován Nimród <morgovannimrod22@gmail.com>
2023-02-22 12:53:25 +0100

    create weight-history

--- backend/src/weight-history/services/weight-history-create/weight-history-create.service.ts
+++ backend/src/weight-history/services/weight-history-create/weight-history-create.service.ts
@@ -1,6 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {PrismaService} from "../../../utils/prirsma.service";
 
 @Injectable()
 export class WeightHistoryCreateService {
+    constructor(private prismaService: PrismaService) {
+    }
+    
+    createWeightHistory(weight: number, dayId: number) {
+        this.prismaService.weightHistory.create({
+            data: {
+                weight: weight,
+                dayId: dayId,
+            },
+        });
+    }
 
 }
