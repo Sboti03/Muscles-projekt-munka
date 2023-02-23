cb5ba0111139c0606a8a1e47bd74ac2b0e818c58
Somogyi Botond <somogyi.botond.munka@gmail.com>
2023-02-23 13:09:41 +0100

    init files

---
+++ backend/src/goals/services/goals-check/goals-check.service.spec.ts
@@ -0,0 +1,28 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import {GoalsCheckService} from "./goals-check.service";
+import {PrismaService} from "../../../utils/prirsma.service";
+import {GoalsGetService} from "../goals-get/goals-get.service";
+
+describe('GoalsCheckService', () => {
+  let service: GoalsCheckService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [GoalsCheckService, PrismaService, GoalsGetService],
+    }).compile();
+
+    service = module.get<GoalsCheckService>(GoalsCheckService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('test', ()=> {
+    it('should be good', async function () {
+      const result = await service.checkGoalsUpdateDto({proteinPerDay: 40, carbohydratesPerDay: 30, targetWeight: 200}, 5)
+      console.log(result)
+    });
+  })
+
+});
