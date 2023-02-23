abfeb7769d1229483bc2e3603e4d11316d815b33
Somogyi Botond <somogyi.botond.munka@gmail.com>
2023-02-23 12:13:48 +0100

    Init goals

--- backend/src/goals/goals.module.ts
+++ backend/src/goals/goals.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
-import { GoalCreateController } from './controllers/goal-create/goal-create.controller';
-import { GoalUpdateController } from './controllers/goal-update/goal-update.controller';
-import { GoalDeleteController } from './controllers/goal-delete/goal-delete.controller';
-import { GoalGetController } from './controllers/goal-get/goal-get.controller';
-import { GoalsGetService } from './controllers/goals-get/goals-get.service';
-import { GoalsUpdateService } from './controllers/goals-update/goals-update.service';
 import { GoalsUpdateService } from './services/goals-update/goals-update.service';
 import { GoalsGetService } from './services/goals-get/goals-get.service';
 import { GoalsConvertService } from './services/goals-convert/goals-convert.service';
+import { GoalsUpdateController } from './controllers/goals-update/goals-update.controller';
+import { GoalsGetController } from './controllers/goals-get/goals-get.controller';
 
 @Module({
-  controllers: [GoalCreateController, GoalUpdateController, GoalDeleteController, GoalGetController],
+  controllers: [
+
+      GoalsUpdateController,
+
+      GoalsGetController],
   providers: [GoalsGetService, GoalsUpdateService, GoalsConvertService]
 })
 export class GoalsModule {}
