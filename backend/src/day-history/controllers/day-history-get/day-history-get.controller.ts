69a57ce9f5df14333d0ce4f9839bf76a6d983ef0
Morgován Nimród <morgovannimrod22@gmail.com>
2023-02-23 15:39:07 +0100

    create controller for day-history-get

--- backend/src/day-history/controllers/day-history-get/day-history-get.controller.ts
+++ backend/src/day-history/controllers/day-history-get/day-history-get.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get, Param} from '@nestjs/common';
+import {DayHistoryGetService} from "../../services/day-history-get/day-history-get.service";
+import {DateParam} from "../../../params/date.param";
+import {GetAndCheckProfileId} from "../../../auth/decorators/decorators";
 
-@Controller('day-history-get')
-export class DayHistoryGetController {}
+@Controller()
+export class DayHistoryGetController {
+    constructor(private dayHistoryGetService: DayHistoryGetService) {
+    }
+
+    @Get('/day-history/weight/?:date')
+    async getWeightByDayIdOrLatestDayId(@Param('date') Currentdate: DateParam, @GetAndCheckProfileId() currentProfileId) {
+        let dayId: number;
+        try {
+            dayId = ((await this.dayHistoryGetService.getDayIdByDate(Currentdate.date, currentProfileId)).dayId);
+        } catch (e) {
+            dayId = (await this.dayHistoryGetService.getLatestDayId()).dayId;
+        }
+        return this.dayHistoryGetService.getWeightByDayId(dayId);
+
+    }
+}
