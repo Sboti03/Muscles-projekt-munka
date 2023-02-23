37a4ec745fd1a725aaafd17b22da3890418f8d4d
Morgován Nimród <morgovannimrod22@gmail.com>
2023-02-23 15:41:00 +0100

    add controller to day history module

--- backend/src/day-history/day-history.module.ts
+++ backend/src/day-history/day-history.module.ts
@@ -2,12 +2,16 @@ import {Module} from '@nestjs/common';
 import {DayHistoryCreateService} from './services/day-history-create/day-history-create.service';
 import {PrismaService} from "../utils/prirsma.service";
 import {DayHistoryGetService} from "./services/day-history-get/day-history-get.service";
+import {DayHistoryGetController} from "./controllers/day-history-get/day-history-get.controller";
 
 @Module({
     providers: [
         DayHistoryCreateService,
         DayHistoryGetService,
         PrismaService
+    ],
+    controllers: [
+        DayHistoryGetController
     ],
     exports: [
         DayHistoryCreateService,
