8e40f7e1f40fecd4cdb663544a863aac5020defc
Somogyi Botond <somogyi.botond.munka@gmail.com>
2023-02-28 18:33:38 +0100

    changes

---
+++ backend/src/meal-history/controllers/meal-history-create/meal-history-create.controller.ts
@@ -0,0 +1,41 @@
+import {Controller, Post} from '@nestjs/common';
+import {GetCurrentUser, GetCurrentUserProfileId} from "../../../auth/decorators/decorators";
+import {RoleEnum} from "../../../Role/utils/roles";
+import {CreateMealHistoryDTO} from "../../dto/createMealHistoryDTO";
+import {PrismaService} from "../../../utils/prirsma.service";
+import {DayHistoryGetService} from "../../../day-history/services/day-history-get/day-history-get.service";
+import {DayHistoryCreateService} from "../../../day-history/services/day-history-create/day-history-create.service";
+import {MealCreateService} from "../../../meal/services/meal-create/meal-create.service";
+import {MealGetService} from "../../../meal/services/meal-get/meal-get.service";
+import {MealHistoryCreateService} from "../../services/meal-history-create/meal-history-create.service";
+import {MealHistoryConvertService} from "../../services/meal-history-convert/meal-history-convert.service";
+import {DayHistoryCheckService} from "../../../day-history/services/day-history-check/day-history-check.service";
+import {MealHistoryGetService} from "../../services/meal-history-get/meal-history-get.service";
+import {MealUpdateService} from "../../../meal/services/meal-update/meal-update.service";
+import {MealHistoryCheckService} from "../../services/meal-history-check/meal-history-check.service";
+
+@Controller('meal-history')
+export class MealHistoryCreateController {
+
+    constructor(private dayHistoryGetService: DayHistoryGetService,
+                private dayHistoryCreateService: DayHistoryCreateService,
+                private mealCreateService: MealCreateService,
+                private mealGetService: MealGetService,
+                private mealHistoryCreateService: MealHistoryCreateService,
+                private mealHistoryConvertService: MealHistoryConvertService,
+                private dayHistoryCheckService: DayHistoryCheckService) {}
+
+    @Post('/create')
+    // @UseGuards(AccessTokenGuard)
+    async createMealHistory(@GetCurrentUser('role') addedBy: RoleEnum,createMealHistoryDTO: CreateMealHistoryDTO, @GetCurrentUserProfileId() profileId: number){
+        const isDayHistoryExist = this.dayHistoryCheckService.checkExistingDayHistory(profileId, createMealHistoryDTO.date)
+        if (!isDayHistoryExist) {
+            await this.dayHistoryCreateService.createDayHistory(profileId, createMealHistoryDTO.date);
+        }
+        const {dayId} = (await this.dayHistoryGetService.getDayIdByDate(createMealHistoryDTO.date, profileId));
+        const mealCreateInput = this.mealGetService.getMealCreateInput(createMealHistoryDTO, addedBy)
+        const {mealId} = await this.mealCreateService.createMeal(mealCreateInput)
+        const mealHistoryCreateInput = await this.mealHistoryConvertService.convertMealHistoryDtoToInput(dayId, mealId, createMealHistoryDTO)
+        return this.mealHistoryCreateService.createMealHistory(mealHistoryCreateInput)
+    }
+}
