a82bcf282537179133e387321e5c9531e0a30475
Morgován Nimród <morgovannimrod22@gmail.com>
2023-03-01 10:46:27 +0100

    admin - delete user by userId in controller

--- backend/src/admin/controllers/admin.controller.ts
+++ backend/src/admin/controllers/admin.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Delete, UseGuards} from '@nestjs/common';
+import {AccessTokenGuard} from "../../auth/guards/access-token.guard";
+import {ProfileGuard} from "../../auth/guards/profile.guard";
+import {AdminBlockService} from "../services/admin-block/admin-block.service";
+import {AdminDeleteService} from "../services/admin-delete/admin-delete.service";
+import {RoleEnum} from "../../Common/Role/utils/roles";
+import {Roles} from "../../Common/Role/decorators/ roles.decorator";
+import {RolesGuard} from "../../auth/guards/role.guard";
 
-@Controller('admin')
-export class AdminController {}
+@UseGuards(AccessTokenGuard, ProfileGuard)
+@Controller('/user/admin')
+export class AdminController {
+    constructor(private adminBlockService: AdminBlockService, private adminDeleteService: AdminDeleteService) {
+    }
+
+    @Roles(RoleEnum.ADMIN)
+    @UseGuards(RolesGuard)
+    @Delete()
+    async deleteUserByUserId(@Body() id: number) {
+        return this.adminDeleteService.deleteUserByUserId(id);
+    }
+
+}
