1b88a2b8af4a120b2a18a1e1df8cf3695cdcbb7f
Morgován Nimród <morgovannimrod22@gmail.com>
2023-03-09 12:50:47 +0100

    refactoring (renaming)

--- desktop/src/main/java/hu/muscles/desktop/LoginController.java
+++ desktop/src/main/java/hu/muscles/desktop/LoginController.java
@@ -3,7 +3,7 @@ package hu.muscles.desktop;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LoginController {
     @FXML
     private Label welcomeText;
 
--- desktop/src/main/java/hu/muscles/desktop/app.java
+++ desktop/src/main/java/hu/muscles/desktop/app.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class app extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(app.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
     }
