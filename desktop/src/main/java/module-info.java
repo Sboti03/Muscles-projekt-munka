36d3c6f86be638d19477a9e757885bb6bcc8f9af
Morgován Nimród <morgovannimrod22@gmail.com>
2023-03-09 12:59:20 +0100

    adding spring and jackson dependencies

--- desktop/src/main/java/module-info.java
+++ desktop/src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module hu.muscles.desktop {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires spring.web;
+    requires spring.core;
+    requires com.fasterxml.jackson.databind;
+
+
     opens hu.muscles.desktop to javafx.fxml;
     exports hu.muscles.desktop;
 }
