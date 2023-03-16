38da733431ffa99d1626e715eacce4b044d4dc3b
Morgován Nimród <morgovannimrod22@gmail.com>
2023-03-16 15:22:03 +0100

    main site basic design

--- desktop/src/main/java/hu/muscles/desktop/urls/Urls.java
+++ desktop/src/main/java/hu/muscles/desktop/urls/Urls.java
@@ -2,11 +2,13 @@ package hu.muscles.desktop.urls;
 
 public class Urls {
 
-    public String LOGIN() {
+    public final String LOGIN() {
         return "http://34.22.242.178:3000/api/auth/login";
     }
 
-    public String LOGOUT() {
+    public final String LOGOUT() {
         return "http://34.22.242.178:3000/api/auth/logut";
     }
+
+    public final String GET_ALL_FOOD() { return "http://34.22.242.178:3000/api/food"; }
 }
