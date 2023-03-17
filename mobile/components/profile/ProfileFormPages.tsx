3d3d051121a7fbdeb4d0dd157a3b49d41b19b102
Kormos Csanad <csanadsuli19@gmail.com>
2023-03-17 16:09:20 +0100

    ProfileFormPages done

--- mobile/components/profile/ProfileFormPages.tsx
+++ mobile/components/profile/ProfileFormPages.tsx
@@ -1,14 +1,13 @@
 import {Button, Flex, Text, TextInput, VStack} from "@react-native-material/core";
-import {useContext, useEffect, useRef, useState} from "react";
-import { StyleSheet, Keyboard, View} from "react-native";
+import {useContext, useRef, useState} from "react";
+import {Keyboard, Platform, StyleSheet, TouchableWithoutFeedback, View} from "react-native";
 import profileProvider, {Profile} from "./ProfileProvider";
-import DatePicker from "react-native-date-picker";
 import NavigatorProvider, {Page} from "../navigator/NavigatorProvider";
 import {homePageStyle} from "../home/homePage";
 import {loginPageStyle} from "../loginPage";
-import {Form, FormItem, Modal} from "react-native-form-component";
-
-
+import {FormItem} from "react-native-form-component";
+import DateTimePicker, {AndroidMode, DateTimePickerAndroid} from '@react-native-community/datetimepicker';
+import RNDateTimePicker from "@react-native-community/datetimepicker";
 
 export interface ProfileProps {
     birthDay?: boolean
@@ -17,9 +16,6 @@ export default function ProfileFormPages (props: ProfileProps) {
 
     const {profile, updateProfile} = useContext(profileProvider)
     const {changePage} = useContext(NavigatorProvider)
-
-    const firstNameRef = useRef<string>('')
-    const lastNameRef = useRef<typeof FormItem>()
 
     const [fNameWarning, setFNameWarning] = useState<string>('')
     const [lNameWarning, setLNameWarning] = useState<string>('')
@@ -33,138 +29,94 @@ export default function ProfileFormPages (props: ProfileProps) {
         if (!profile?.lastName) {
             setLNameWarning('This field is required')
         }else if (profile.lastName) setLNameWarning('')
-        else {
-
+        if (profile!.firstName && profile!.lastName) {
+            changePage(Page.BIRTHDAYANDWEIGHT)
+            console.log(profile)
         }
     }
-    const clearWarning = async () => {
-        await console.log('elotte')
+    const clearWarning = () => {
         if (profile?.firstName) setFNameWarning('')
         if (profile?.lastName) setLNameWarning('')
-        console.log('utana')
     }
 
-    useEffect(()=> {
-        console.log(profile)
-    },[profile])
-
-
-    const updateSearchText = (text: string) => {
-        // console.log(firstNameRef.current?.propTypes?.value?.name.toString()!)
-        console.log(text)
-        return text
-    };
+    function showDatePicker() {
+        DateTimePickerAndroid.open({
+            value: profile?.birthDay!,
+            onChange: event => updateProfile({...profile, birthDay: new Date(event.nativeEvent.timestamp!)}),
+            mode: 'date',
+            display: 'spinner',
+            maximumDate: new Date(),
+            minimumDate: new Date('1900-01-01'),
+        })
+    }
 
     if (props.birthDay){
         return (
-            <VStack>
-                <DatePicker date={new Date()}
-                            onDateChange={(date) => {
-                                const currentProfileFirstname: Profile = {birthDay: date}
-                                updateProfile(currentProfileFirstname)
-                            }}
-                            maximumDate={new Date()}
-                            mode={"date"}
-                />
-                <Text></Text>
-                <Text></Text>
-                <TextInput placeholder={'height'}
-                           style={loginPageStyle.input}
-                           keyboardType={"numeric"}
-                           maxLength={3}
-                           onEndEditing={event => {
-                               const height = Number(event.nativeEvent.text)
-                               updateProfile({...profile, height})
-                           }}
-                           onBlur={clearWarning}
-                           helperText={heightWarning}
-                />
-                    <Button title={'Next'}
-                            onPress={() => {
-                                changePage(Page.HOME)
-                            }}
+            <Flex fill style={homePageStyle.page}>
+                <VStack style={profileFormStyles.container}>
+                    <Text>{profile?.birthDay?.toLocaleDateString()}</Text>
+                    <Text onPress={showDatePicker}>change birthday</Text>
+                    <TextInput placeholder={'height'}
+                               style={Object.assign({}, profileFormStyles.inputOnTheMiddle ,loginPageStyle.input)}
+                               inputStyle={{backgroundColor: '#aa8dff'}}
+                               placeholderTextColor={'#d2bdfc'}
+                               keyboardType={"numeric"}
+                               maxLength={3}
+                               onChangeText={inputHeight => {
+                                   const height = Number(inputHeight)
+                                   updateProfile({...profile, height})
+                                   clearWarning()
+                               }}
+                               helperText={heightWarning}
                     />
-            </VStack>
+                        <Button title={'Next'}
+                                style={profileFormStyles.buttons}
+                                onPress={() => {
+                                    changePage(Page.HOME)
+                                }}
+                        />
+                </VStack>
+            </Flex>
         )
     }
     else return (
-        <Flex fill style={homePageStyle.page}>
-            <VStack  style={profileFormStyles.container}>
-                <Form onButtonPress={() => {
-                    console.log(firstNameRef.current)
-                    console.log('pressed')
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <Flex fill style={homePageStyle.page}>
+                <VStack  style={profileFormStyles.container}>
+                    <Text style={profileFormStyles.texts}>
+                        First name
+                    </Text>
+                    <TextInput placeholder={'Firstname'}
+                               style={Object.assign({}, profileFormStyles.inputOnTheMiddle ,loginPageStyle.input)}
+                               inputStyle={{backgroundColor: '#aa8dff'}}
+                               placeholderTextColor={'#d2bdfc'}
+                               onChangeText={firstName =>{
+                                   updateProfile({...profile, firstName})
+                                   clearWarning()
+                               }}
+                               helperText={fNameWarning}
 
-                }}
-                      buttonStyle={profileFormStyles.buttons}
-
-                >
-                    <FormItem value={profile?.firstName!}
-                              isRequired={true}
-                              label={'First name'}
-                              onChangeText={(text: string) => {updateSearchText(text)}}
-                              onChange={() => {
-                               updateProfile({...profile, firstName: firstNameRef.current})
-                              }}
-                              // onChange={e => {
-                              //     console.log(firstNameRef.current)
-                              // }}
-                              ref={firstNameRef}
                     />
-                    <FormItem value={'alma'}
-                              isRequired={true}
-                              label={'Last name'}
-                              ref={lastNameRef}
-                              onChangeText={text => {
-                                  console.log(lastNameRef.current)
-                                  console.log(text)
-                              }}
-                              onEndEditing={event => {
-
-                                  console.log(lastNameRef.current)
-                                  //updateProfile({...profile, lastName: event.nativeEvent.text})
-                              }}
+                    <Text style={profileFormStyles.texts}>
+                        Last name
+                    </Text>
+                    <TextInput placeholder={'Lastname'}
+                               style={Object.assign({}, profileFormStyles.inputOnTheMiddle ,loginPageStyle.input)}
+                               inputStyle={{backgroundColor: '#aa8dff'}}
+                               placeholderTextColor={'#d2bdfc'}
+                               onChangeText={lastName =>{
+                                   updateProfile({...profile, lastName})
+                                   clearWarning()
+                               }}
+                               helperText={lNameWarning}
                     />
-                </Form>
-                <Text style={profileFormStyles.texts}>
-                    First name
-                </Text>
-                <TextInput placeholder={'Firstname'}
-                           style={Object.assign({}, profileFormStyles.inputOnTheMiddle ,loginPageStyle.input)}
-                           inputStyle={{backgroundColor: '#aa8dff'}}
-                           placeholderTextColor={'#d2bdfc'}
-                           onEndEditing={event => {
-                               const firstName = event.nativeEvent.text
-                               updateProfile({...profile, firstName})
-                           }}
-                           onBlur={clearWarning}
-                           helperText={fNameWarning}
-
-                />
-                <Text style={profileFormStyles.texts}>
-                    Last name
-                </Text>
-                <TextInput placeholder={'Lastname'}
-                           style={Object.assign({}, profileFormStyles.inputOnTheMiddle ,loginPageStyle.input)}
-                           inputStyle={{backgroundColor: '#aa8dff'}}
-                           placeholderTextColor={'#d2bdfc'}
-                           onEndEditing={event => {
-                               const lastName = event.nativeEvent.text
-                               updateProfile({...profile, lastName})
-
-                           }}
-                           onBlur={clearWarning}
-                           helperText={lNameWarning}
-                />
-                <Button title={'Next'}
-                        style={profileFormStyles.buttons}
-
-                        onPress={() => {
-                            submit()
-                        }}
-                />
-            </VStack>
-            <Text>{profile?.firstName + ' ' + profile?.lastName}</Text>
-        </Flex>
+                    <Button title={'Next'}
+                            style={profileFormStyles.buttons}
+                            onPress={submit}
+                    />
+                </VStack>
+            </Flex>
+        </TouchableWithoutFeedback>
     )
 }
 const profileFormStyles = StyleSheet.create({
@@ -181,7 +133,7 @@ const profileFormStyles = StyleSheet.create({
         alignItems:"center"
     },
     buttons: {
-
+        marginTop: 30,
         width: 200,
     },
     texts: {
