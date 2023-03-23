a7a2da150c0b1e9698346b6d3f247c769ee1b79f
Morgován Nimród <morgovannimrod22@gmail.com>
2023-03-23 10:00:43 +0100

    fixing selection in case of error

--- desktop/src/main/java/hu/muscles/desktop/controllers/MainViewController.java
+++ desktop/src/main/java/hu/muscles/desktop/controllers/MainViewController.java
@@ -62,6 +62,8 @@ public class MainViewController implements Initializable {
     private final String[] profileDataString = new String[]{"First name", "Last name", "Birthdate", "Registration date", "Height", "Last changed"};
 
     private boolean isProfileShown = false;
+    private boolean isFoodShown = false;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -72,7 +74,7 @@ public class MainViewController implements Initializable {
         confirmExit.setHeaderText("Are you sure you want to exit the app?");
         mainListView.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
             foodEditLabelText.getItems().clear();
-            if (!isProfileShown) {
+            if (!isProfileShown && isFoodShown) {
                 foodEditLabelText.getItems().addAll(updateFoodDataString);
                 if (!mainListView.getSelectionModel().isEmpty()) {
                     foodEditText.getItems().clear();
@@ -80,7 +82,7 @@ public class MainViewController implements Initializable {
                     foodEditText.setEditable(true);
                 }
             }
-            if (isProfileShown) {
+            if (!isFoodShown && isProfileShown) {
                 foodEditLabelText.getItems().addAll(profileDataString);
                 if (!mainListView.getSelectionModel().isEmpty()) {
                     foodEditText.getItems().clear();
@@ -109,18 +111,21 @@ public class MainViewController implements Initializable {
 
     @FXML
     public void profilesClick(ActionEvent actionEvent) {
-        isProfileShown = true;
+        isFoodShown = false;
         mainListView.getSelectionModel().clearSelection();
+        mainListView.getItems().clear();
         profiles = loadAllProfile();
         try {
             if (profiles != null) {
                 loadProfilesToListView(profiles);
+                isProfileShown = true;
             } else {
                 foodEditLabelText.getItems().add("Couldn't read profiles.");
             }
         } catch (Exception e) {
             foodEditText.getItems().clear();
             foodEditText.getItems().add(e.getMessage());
+            foodEditLabelText.getItems().add("Couldn't read profiles.");
             e.printStackTrace();
         }
     }
@@ -141,16 +146,19 @@ public class MainViewController implements Initializable {
     public void foodsClick(ActionEvent actionEvent) {
         isProfileShown = false;
         mainListView.getSelectionModel().clearSelection();
+        mainListView.getItems().clear();
         foods = loadAllFood();
         try {
             if (foods != null) {
                 loadFoodsToListView(foods);
+                isFoodShown = true;
             } else {
                 foodEditLabelText.getItems().add("Couldn't read foods.");
             }
         } catch (Exception e) {
             foodEditText.getItems().clear();
             foodEditText.getItems().add(e.getMessage());
+            foodEditLabelText.getItems().add("Couldn't read foods.");
             e.printStackTrace();
         }
     }
