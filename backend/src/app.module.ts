c3a1eed84f22145d300209a02e11a3a9b4496420
Somogyi Botond <somogyi.botond.munka@gmail.com>
2023-04-14 10:45:37 +0200

    Added frontend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -20,7 +20,15 @@ import {join} from "path";
 
 @Module({
     imports: [
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', 'client'),
+            exclude: ['/api/(.*)'],
+        }),
         UserModule,
+        DevtoolsModule.register({
+            http: process.env.NODE_ENV !== 'prod',
+            port: 8000
+        }),
         AuthModule,
         ProfileModule,
         ConfigModule.forRoot({
