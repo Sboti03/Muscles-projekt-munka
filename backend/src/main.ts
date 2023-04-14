ddc769953d0e2591a254f859fb53e8a5f5e883b9
Somogyi Botond <somogyi.botond.munka@gmail.com>
2023-04-14 10:48:31 +0200

    Added frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
     SwaggerModule.setup('api', app, document);
     app.use(cookieParser())
 
-    await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3030);
+    await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
 }
 
 bootstrap();
