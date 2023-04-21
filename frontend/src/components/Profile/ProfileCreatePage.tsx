9c7d059c280cd83972b04d1f5fa1c81500487f08
Somogyi Botond <somogyi.botond.munka@gmail.com>
2023-04-21 09:26:52 +0200

    fixed btn and new build

--- frontend/src/components/Profile/ProfileCreatePage.tsx
+++ frontend/src/components/Profile/ProfileCreatePage.tsx
@@ -3,10 +3,12 @@ import ProfileDataView from "./ProfileDataView/ProfileDataView";
 import GoalsDataView from "./GoalsDataView/GoalsDataView";
 import NavigatorContext, {Page} from "../Navigator/NavigatorContext";
 import {signal} from "@preact/signals-react";
+import AuthContext from "../Auth/AuthContext";
+import {RoleEnum} from "../Types/Role";
 
 
 export default function ProfileCreatePage() {
-
+    const {user} = useContext(AuthContext)
     const profile = <>
         <div className="max-w-md m-auto">
             <ProfileDataView saveBtn={"Next"} backBtn={undefined} saveBtnAction={next}/>
@@ -26,7 +28,11 @@ export default function ProfileCreatePage() {
     function next() {
         renderState.value++
         if (renderState.value === renderList.length) {
-            changePage(Page.HOME)
+            if (user?.role.roleName === RoleEnum.COACH) {
+                changePage(Page.COACH_HOME)
+            } else {
+                changePage(Page.HOME)
+            }
         } else {
             setRender(renderList[renderState.value])
         }
